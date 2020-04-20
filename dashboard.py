9d35d17adb3a4ca9129acccdb345cfb3b5c07f48
EvelinaBodnar <43322909+EvelinaBodnar@users.noreply.github.com>
2020-04-20 11:50:59 +0300

    Add files via upload

---
+++ dashboard.py
@@ -0,0 +1,137 @@
+import cx_Oracle
+
+import chart_studio
+chart_studio.tools.set_credentials_file(username='Evelinabodnar', api_key='S6Cu2THhk1VIBPXEKZVf')
+conn = cx_Oracle.connect("SYS","2805052001","lockalhost/xe")
+cur = conn.cursor()
+
+cur.execute('''
+SELECT ROUND(AVG(rating),3) AS rating
+, TRIM(company) AS company
+FROM Chocolate
+INNER JOIN Company
+ON Chocolate.company= Company.company 
+GROUP BY Company
+ORDER BY  rating DESC;
+''')
+rows = cur.fetchall()
+x = []
+y = []
+for row in rows:
+    x.append(row[0])
+    y.append(row[1])
+print(x, y)
+
+import plotly.graph_objects as go
+import chart_studio.plotly as py
+bar = [go.Bar(x=y, y=x)]
+layout = go.Layout(
+    title='Rating of different chocolates',
+    xaxis=dict(
+        title='Chocolate',
+        titlefont=dict(
+            family='Courier New, monospace',
+            size=16,
+            color='#7f7f70'
+        )
+    ),
+    yaxis=dict(
+        title='Rating per chocolate',
+        rangemode='nonnegative',
+        autorange=True,
+        titlefont=dict(
+            family='Courier New, monospace',
+            size=16,
+            color='#7f7f70'
+        )
+    )
+)
+fig = go.Figure(data=bar, layout=layout)
+
+
+bar_rating_url=py.plot(fig,filename='Rating of different chocolates', auto_open=False)
+
+cur.execute('''
+SELECT company, ROUND(COUNT(bar_name)*100/t.count, 2) AS persent
+FROM Chocolate,
+    (SELECT COUNT(bar_name) AS count
+FROM Chocolate)t   
+GROUP BY  company,
+     t.count; 
+''')
+
+
+rows = cur.fetchall()
+x = []
+y = []
+for row in rows:
+    x.append(row[0])
+    y.append(row[1])
+print(x, y)
+pie = go.Figure(data=[go.Pie(labels=y, values=x )])
+pie_choc_url=py.plot(pie, filename='percentage of chocolate company ',auto_open=False)
+
+cur.execute('''   SELECT Bean.bean_type,
+    COUNT(Chocolate.diamond_index) AS count
+    FROM Chocolate 
+    INNER JOIN Bean ON Chocolate.bean_type = Bean.bean_type
+GROUP BY Bean.bean_type
+ORDER BY count DESC;
+''')
+
+rows = cur.fetchall()
+x = []
+y = []
+for row in rows:
+    x.append(row[0])
+    y.append(row[1])
+print(x, y)
+
+scatter = go.Figure([go.Scatter(x=y, y=x)])
+
+scatter_beans_url=py.plot(scatter, filename = '
+dependence on the type of beans on the percentage of chocolate, auto_open=False)
+
+import re
+import chart_studio.dashboard_objs as dashboard
+def fileId_from_url(url):
+    raw_fileId = re.findall("~[A-z.]+/[0-9]+", url)[0][1: ]
+    return raw_fileId.replace('/', ':')
+
+my_dboard = dashboard.Dashboard()
+bar_rating= fileId_from_url(bar_rating_url)
+pie_choc= fileId_from_url(pie_choc_url)
+scatter_beans= fileId_from_url(scatter_beans_url)
+
+
+
+box_1 = {
+    'type': 'box',
+    'boxType': 'plot',
+    'fileId':bar_rating,
+    'title': 'coast of land in different regions'
+}
+
+box_2 = {
+    'type': 'box',
+    'boxType': 'plot',
+    'fileId':pie_choc,
+    'title': 'The sum of all the GDP of region realetive to the GDP of the Earth '
+}
+
+box_3 = {
+    'type': 'box',
+    'boxType': 'plot',
+    'fileId':scatter_beans ,
+    'title': 'Level of migration above the regions'
+}
+
+my_dboard.insert(box_1)
+my_dboard.insert(box_2, 'below', 1)
+my_dboard.insert(box_3, 'left', 2)
+
+py.dashboard_ops.upload(my_dboard, 'Dashboard')
+
+conn.commit()
+cur.close()
+conn.close()
