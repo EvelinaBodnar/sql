9003ed992d63658a14d0b895babd4d3c29a330d6
EvelinaBodnar <43322909+EvelinaBodnar@users.noreply.github.com>
2020-04-20 11:01:28 +0300

    Add files via upload

---
+++ populate.sql
@@ -0,0 +1,65 @@
+INSERT INTO Chocolate (bar_name, company, bean_type, cocoa_perc, rating) 
+VALUES ('Madagascar', 'Soma', 'Trinitario', 70, 3.75);
+
+INSERT INTO Chocolate (bar_name, company, bean_type, cocoa_perc, rating) 
+VALUES ('Peru ', 'Bonnat', 'Trinitario', 70, 2.0);
+
+INSERT INTO Chocolate (bar_name, company, bean_type, cocoa_perc, rating) 
+VALUES ('Ecuador ', 'Soma', '', 70, 4.8);
+
+INSERT INTO Chocolate (bar_name, company, bean_type, cocoa_perc, rating) 
+VALUES ('Dominican Republic ', 'Bonnat', 'Criollo', 75, 3.2);
+
+INSERT INTO Chocolate (bar_name, company, bean_type, cocoa_perc, rating) 
+VALUES ('Kpime ', 'Fresco','', 75, 2.4);
+
+INSERT INTO Chocolate (bar_name, company, bean_type, cocoa_perc, rating) 
+VALUES ('Atsane ', 'Pralus', 'Trinitario', 72, 1.2);
+
+INSERT INTO Chocolate (bar_name, company, bean_type, cocoa_perc, rating) 
+VALUES ('Quilla ', 'Soma', 'Criollo', 72, 4.1);
+
+INSERT INTO Chocolate (bar_name, company, bean_type, cocoa_perc, rating) 
+VALUES ('Carenero ', 'Pralus', '', 72, 2.2);
+
+INSERT INTO Chocolate (bar_name, company, bean_type, cocoa_perc, rating) 
+VALUES ('Akata ', 'A.Morin', 'Forastero', 65, 4.3);
+
+INSERT INTO Chocolate (bar_name, company, bean_type, cocoa_perc, rating) 
+VALUES ('Agua Grande ', 'Soma', 'Trinitario', 45, 3.3);
+
+
+
+
+
+INSERT INTO Company (company) 
+VALUES ('Soma');
+
+INSERT INTO Company (company) 
+VALUES ('Bonnat');
+
+INSERT INTO Company (company) 
+VALUES ('A.Morin');
+
+
+INSERT INTO Company (company) 
+VALUES ('Pralus');
+
+INSERT INTO Company (company) 
+VALUES ('Fresco');
+
+
+
+INSERT INTO Bean (bean_type) 
+VALUES ('Trinitario');
+
+INSERT INTO Bean (bean_type) 
+VALUES ('Criollo');
+
+INSERT INTO Bean(bean_type) 
+VALUES ('Forastero');
+
+
+
+
+
