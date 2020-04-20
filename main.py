0161093b0203169ac730b56a3bce3784717f3174
EvelinaBodnar <43322909+EvelinaBodnar@users.noreply.github.com>
2020-04-20 11:50:44 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,43 @@
+import cx_Oracle
+
+conn = cx_Oracle.connect("SYS","2805052001","lockalhost/xe")
+cur = conn.cursor()
+
+cur.execute('''
+SELECT ROUND(AVG(rating),3) AS rating
+, TRIM(company) AS company
+FROM Chocolate
+INNER JOIN Company
+ON Chocolate.company= Company.company 
+GROUP BY Company
+ORDER BY  rating DESC;
+''')
+
+rows = cur.fetchall()
+print(rows)
+
+cur.execute('''
+SELECT company, ROUND(COUNT(bar_name)*100/t.count, 2) AS persent
+FROM Chocolate,
+    (SELECT COUNT(bar_name) AS count
+FROM Chocolate)t   
+GROUP BY  company,
+     t.count; 
+''')
+
+rows = cur.fetchall()
+print(rows)
+
+cur.execute('''   SELECT Bean.bean_type,
+    COUNT(Chocolate.diamond_index) AS count
+    FROM Chocolate 
+    INNER JOIN Bean ON Chocolate.bean_type = Bean.bean_type
+GROUP BY Bean.bean_type
+ORDER BY count DESC;
+''')
+
+rows = cur.fetchall()
+print(rows)
+conn.commit()
+cur.close()
+conn.close()
